56d854f582ae95493e8b34aba2366fd3c745a973
Jonathan Mather <matherj@flip1.engr.oregonstate.edu>
2021-06-06 23:40:26 -0700

    actually fixed linting errors

--- test_calc.py
+++ test_calc.py
@@ -5,9 +5,8 @@ import calc
 
 
 class TestCalcApp:
-	def test_add(self):
-		assert 6 == calc.add(2,4)
+    def test_add(self):
+        assert 6 == calc.add(2, 4)
 
-
-	def test_subtract(self):
-		assert 6 == calc.subtract(8,2)
+    def test_subtract(self):
+        assert 6 == calc.subtract(8, 2)
